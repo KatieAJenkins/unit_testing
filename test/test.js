db5fabf02e70a5df65c90349ba79f55f8d1817d0
KatieAJenkins <kind2karma@gmail.com>
2017-02-15 16:03:18 -0700

    initial setup complete

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+var expect = require('chai').expect;
+var code = require('../main.js');
+
+// Input: name, string (ex: 'Jennie')
+//
+// Output: greeting that incorporates string input (ex: 'Hello, Jennie')
+//
+// Test for:
+//
+// accepts only one argument
+// input is a string
+// output is a string
+// output is expected string
